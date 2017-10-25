87dbca2ae55468f9a7ee583441246739979123d0
Alberto Ivo Vieira <albertoivo@gmail.com>
2017-10-25 13:43:42 -0200

    refactoring and prettier

--- src/Shelf.js
+++ src/Shelf.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React from 'react'
 
 const Shelf = ({ title, books, update }) => {
   return (
@@ -6,45 +6,42 @@ const Shelf = ({ title, books, update }) => {
       <h2 className="bookshelf-title">{title}</h2>
       <div className="bookshelf-books">
         <ol className="books-grid">
-          {(!books.error || books) &&
-            books.map(book => (
-              <li key={book.id}>
-                <div className="book">
-                  <div className="book-top">
-                    <div
-                      className="book-cover"
-                      style={{
-                        width: 128,
-                        height: 193,
-                        backgroundImage: `url(${book.imageLinks !== undefined
-                          ? book.imageLinks.smallThumbnail
-                          : ""})`
-                      }}
-                    />
-                    <div className="book-shelf-changer">
-                      <select
-                        value={book.shelf || "none"}
-                        onChange={event => update(book, event.target.value)}
-                      >
-                        <option value="" disabled>
-                          Move to...
-                        </option>
-                        <option value="currentlyReading">
-                          Currently Reading
-                        </option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="book-title">{book.title}</div>
-                  <div className="book-authors">
-                    {book.authors === undefined || book.authors.join(", ")}
+          {books.map(book => (
+            <li key={book.id}>
+              <div className="book">
+                <div className="book-top">
+                  <div
+                    className="book-cover"
+                    style={{
+                      width: 128,
+                      height: 193,
+                      backgroundImage: `url(${book.imageLinks.smallThumbnail})`
+                    }}
+                  />
+                  <div className="book-shelf-changer">
+                    <select
+                      value={book.shelf || 'none'}
+                      onChange={event => update(book, event.target.value)}
+                    >
+                      <option value="" disabled>
+                        Move to...
+                      </option>
+                      <option value="currentlyReading">
+                        Currently Reading
+                      </option>
+                      <option value="wantToRead">Want to Read</option>
+                      <option value="read">Read</option>
+                      <option value="none">None</option>
+                    </select>
                   </div>
                 </div>
-              </li>
-            ))}
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">
+                  {book.authors === undefined || book.authors.join(', ')}
+                </div>
+              </div>
+            </li>
+          ))}
         </ol>
       </div>
     </div>
