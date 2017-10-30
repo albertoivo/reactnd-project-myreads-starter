bc0679a45d06bced399b3865f1ffe9529efa96dd
Alberto Ivo Vieira <albertoivo@gmail.com>
2017-10-30 20:53:26 -0200

    Shelf utilizando o novo component de Book

--- src/Shelf.js
+++ src/Shelf.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Book from './Book'
 
 const Shelf = ({ title, books, update }) => {
   return (
@@ -8,37 +9,7 @@ const Shelf = ({ title, books, update }) => {
         <ol className="books-grid">
           {books.map(book => (
             <li key={book.id}>
-              <div className="book">
-                <div className="book-top">
-                  <div
-                    className="book-cover"
-                    style={{
-                      width: 128,
-                      height: 193,
-                      backgroundImage: `url(${book.imageLinks.smallThumbnail})`
-                    }}
-                  />
-                  <div className="book-shelf-changer">
-                    <select
-                      value={book.shelf || 'none'}
-                      onChange={event => update(book, event.target.value)}>
-                      <option value="" disabled>
-                        Move to...
-                      </option>
-                      <option value="currentlyReading">
-                        Currently Reading
-                      </option>
-                      <option value="wantToRead">Want to Read</option>
-                      <option value="read">Read</option>
-                      <option value="none">None</option>
-                    </select>
-                  </div>
-                </div>
-                <div className="book-title">{book.title}</div>
-                <div className="book-authors">
-                  {book.authors === undefined || book.authors.join(', ')}
-                </div>
-              </div>
+              <Book book={book} update={update} />
             </li>
           ))}
         </ol>
