1c4a530262c196bcdadd8113b238a135a18e625b
Alberto Ivo Vieira <albertoivo@gmail.com>
2017-10-31 13:15:04 -0200

    formating...

--- src/Book.js
+++ src/Book.js
@@ -3,36 +3,31 @@ import React from 'react'
 const Book = ({ book, update }) => {
   return (
     <div className="book">
-    <div className="book-top">
-      <div
-        className="book-cover"
-        style={{
-          width: 128,
-          height: 193,
-          backgroundImage: `url(${book.imageLinks.smallThumbnail})`
-        }}
-      />
-      <div className="book-shelf-changer">
-        <select
-          value={book.shelf || 'none'}
-          onChange={event => update(book, event.target.value)}>
-          <option value="" disabled>
-            Move to...
-          </option>
-          <option value="currentlyReading">
-            Currently Reading
-          </option>
-          <option value="wantToRead">Want to Read</option>
-          <option value="read">Read</option>
-          <option value="none">None</option>
-        </select>
+      <div className="book-top">
+        <div
+          className="book-cover"
+          style={{
+            width: 128, height: 193,
+            backgroundImage: `url(${book.imageLinks.smallThumbnail})`
+          }}
+        />
+        <div className="book-shelf-changer">
+          <select
+            value={book.shelf || 'none'}
+            onChange={event => update(book, event.target.value)}>
+            <option value="" disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
+        </div>
+      </div>
+      <div className="book-title">{book.title}</div>
+      <div className="book-authors">
+        {book.authors === undefined || book.authors.join(', ')}
       </div>
     </div>
-    <div className="book-title">{book.title}</div>
-    <div className="book-authors">
-      {book.authors === undefined || book.authors.join(', ')}
-    </div>
-  </div>
   )
 }
 
