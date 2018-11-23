88cf3619130138189dc44e851a5da86251e4a276
Alberto <alberto.vieira@serpro.gov.br>
2018-11-23 07:33:59 -0200

    add linkedIn

--- src/Footer.js
+++ src/Footer.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import {
-  FaFacebookSquare,
-  FaInstagram,
-  FaGithub
+  FaFacebook, FaInstagram, FaGithub, FaLinkedinIn
 } from 'react-icons/fa'
 
 const Footer = () => (
   <footer>
     <p>Find me on social media</p>
-    <a href="https://www.facebook.com/nossasrotass" target="_blank" rel="noopener noreferrer"><FaFacebookSquare /></a>
+    <a href="https://www.facebook.com/nossasrotass" target="_blank" rel="noopener noreferrer"><FaFacebook /></a>
     <a href="https://instagram.com/nossasrotas" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
     <a href="https://github.com/albertoivo" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
+    <a href="https://www.linkedin.com/in/alberto-ivo-vieira" target="_blank" rel="noopener noreferrer"><FaLinkedinIn /></a>
     <p>
       Powered by <a href="https://albertoivo.github.io" target="_blank" rel="noopener noreferrer">
         AlbertoIvo.github.io</a>
