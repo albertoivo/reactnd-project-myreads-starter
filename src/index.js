20fb20ae84eaeb3d62315619f1608a28fd120d8a
Alberto <alberto.vieira@serpro.gov.br>
2018-11-26 14:58:13 -0200

    service-wroker

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 import App from './App'
 import './index.css'
-import registerServiceWorker from './registerServiceWorker'
+import './start-service-worker'
 
 ReactDOM.render(
   <BrowserRouter basename={process.env.PUBLIC_URL + '/'}>
@@ -11,8 +11,3 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root')
 )
-registerServiceWorker()
-/* the application itself reloads, but not the page. */
-//if (module.hot) {
-//  module.hot.accept();
-//}
